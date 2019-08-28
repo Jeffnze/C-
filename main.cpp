6d422c8a25ecc09d997cdfd239d1ca66e7035aa3
Jeffnze <52266988+Jeffnze@users.noreply.github.com>
2019-08-28 09:57:35 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    cin>> a;
+    int cont = 2;
+    while (a>0){
+        if (a%cont==0)
+            cout<< a << "no es primo" << endl;
+        else
+            cout<< a << "es primo" << endl;
+            m = m + cont;
+
+
+    }
+    cout << "Hello world!" << endl;
+    return 0;
+}
